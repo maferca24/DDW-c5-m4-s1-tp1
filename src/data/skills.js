152fdef2cdbc23e23c0eeb0f2bf0348948decc36
maferca24 <mafercarrenio@gmail.com>
2026-08-20 12:18:55 -0300

    cree archivo skills.js

---
+++ src/data/skills.js
@@ -0,0 +1,6 @@
+export const skills = [
+  { id: 1, name: "HTML",       level: "Avanzado",    isFavorite: false },
+  { id: 2, name: "CSS",        level: "Intermedio",  isFavorite: false },
+  { id: 3, name: "JavaScript", level: "Intermedio",  isFavorite: true  },
+  { id: 4, name: "React",      level: "Aprendiendo", isFavorite: true  },
+];
