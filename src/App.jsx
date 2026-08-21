6b1073838870599bbfbd06ce9dd13f0343846b03
maferca24 <mafercarrenio@gmail.com>
2026-08-21 09:32:48 -0300

    agregue imagen

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import './App.css'
 function App() {
   const name = "Fernanda"; // 
   const phrase = "Hola, soy " + name + " y estoy comenzando en este mundo fascinante del desarrollo web"; //
+  const avatar = "/src/assets/avatar_mfc.png"; //
   
   return (
     <>
       <h1>{phrase}</h1>
+      <img src={avatar} alt="Avatar de Fernanda" />
       <p>Este es mi primer trabajo práctico de React</p>
 
     </>
