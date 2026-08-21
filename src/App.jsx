2a22694385a1553b26cf5a340d23963d493235e1
maferca24 <mafercarrenio@gmail.com>
2026-08-21 09:23:43 -0300

    modifique app, frase y nombre

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const name = "Fernanda"; // 
+  const phrase = "Hola, soy " + name + " y estoy comenzando en este mundo fascinante del desarrollo web"; //
+  
   return (
     <>
-     </>
+      <h1>{phrase}</h1>
+      <p>Este es mi primer trabajo práctico de React</p>
+
+    </>
   )
 }
 
