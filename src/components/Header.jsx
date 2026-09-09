2e2bd0edc5a2578501f84e812f0df623d1eab48a
MAFERCARRE <mafercarrenio@gmail.com>
2026-09-08 21:25:39 -0300

    Elimine componente Header no utilizado

--- src/components/Header.jsx
+++
@@ -1,8 +0,0 @@
-export default function Header() {
-  return (
-     <header className="bg-sky-400 " > 
-      <p>Trabajo Practico N°1 - Trabajar con componentes y props sin distracciones</p>
-    </header>
-  )
-}
-
